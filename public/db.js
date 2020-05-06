68d277fd365dd1929c4576abff8eb0e58c3ce5f2
Keaton Smith <keatonpsmith55@gmail.com>
2020-05-05 22:26:31 -0700

    service worker

--- public/db.js
+++ public/db.js
@@ -27,14 +27,59 @@ request.onsuccess = function(event) {
 };
 
 // TODO: log any indexedDB errors
+request.onerror = function(event) {
+  console.log(`Woops! ${ event.target.errorCode}`);
+};
 
 // TODO: add code so that any transactions stored in the db
 // are sent to the backend if/when the user goes online
-// Hint: learn about "navigator.onLine" and the "online" window event.
+// Hint: learn about "navigator.onLine" and the "online" window event
+function checkDatabase() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(["pending"], "readwrite");
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+  // get all records from store and set to a variable
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        // if successful, open a transaction on your pending db
+        const transaction = db.transaction(["pending"], "readwrite");
+
+        // access your pending object store
+        const store = transaction.objectStore("pending");
+
+        // clear all items in your store
+        store.clear();
+      });
+    }
+  };
+}
 
 // TODO: add code to saveRecord so that it accepts a record object for a
 // transaction and saves it in the db. This function is called in index.js
 // when the user creates a transaction while offline.
 function saveRecord(record) {
   // add your code here
+  // create a transaction on the pending db with readwrite access
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+ 
+  // add record to your store with add method.
+  store.add(record);
 }
+
+window.addEventListener("online", checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -4,3 +4,83 @@
 // when the app is used offline. HINT: You should use two caches. One for the
 // static assets such ass html, css, js, images, etc; and another cache for
 // the dynamic data from requests to routes beginning with "/api".
+const FILES_TO_CACHE = [
+    "/",
+    "/db.js",
+    "/index.js",
+    "/index.html",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  ];
+  
+  const CACHE_NAME = "static-cache-v1";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+
+  self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", (event) => {
+    event.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("clearing cache", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  
+  self.addEventListener("fetch", (event) => {
+    
+    if (event.request.url.includes("/api/") && event.request.method === "GET") {
+      event.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then((cache) => {
+            return fetch(event.request)
+              .then((response) => {
+              
+                if (response.status === 200) {
+                  cache.put(event.request, response.clone());
+                }
+  
+                return response;
+              })
+              .catch(() => {
+              
+                return cache.match(event.request);
+              });
+          })
+          .catch((err) => console.log(err))
+      );
+  
+      
+      return;
+    }
+  
+
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
